09f1a15fcd87ace42f15758d39bc0e3d387b8e3d
gabrielcedran <gabrielcedran@gmail.com>
2022-07-16 22:48:20 +0100

    Bootstrap basic components to fetch and list result

---
+++ components/SearchResults.tsx
@@ -0,0 +1,22 @@
+import { ProductItem } from "./ProductItem";
+
+type SearchResults = {
+    results: Array<{
+        id: number;
+        price: number;
+        title: string;
+    }>
+
+}
+
+export function SearchResults({results}: SearchResults) {
+    return (
+        <div>
+            {results.map(product => {
+                return (
+                    <ProductItem key={product.id} product={product}/>
+                );
+            })}
+        </div>
+    )
+}
